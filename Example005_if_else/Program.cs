383f34ec7aab3f0a11bf2210fe6efca0d350de08
olya <abc@mail.ru>
2022-12-16 15:23:55 +0300

    add example005_if_else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "оля")
+{
+    Console.WriteLine("Ура, это же ОЛЯ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
